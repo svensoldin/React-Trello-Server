ee01c1e23aaeb46866b2f84dadd8f70fe82a751c
Sven Soldin <soldinsven@gmail.com>
2020-11-07 14:15:14 +0100

    initial commit, created Board and User models

---
+++ README.md
@@ -0,0 +1,3 @@
+Backend code for the Thullo app.
+
+Trello Clone Challenge: https://devchallenges.io/challenges/wP0LbGgEeKhpFHUpPpDh
---
+++ models/Board.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BoardSchema = new Schema({
+	users: {
+		type: Schema.Types.ObjectId,
+		ref: "users",
+	},
+	admins: {
+		type: Schema.Types.ObjectId,
+		ref: "users",
+	},
+	columns: [
+		{
+			title: String,
+			cards: [
+				{
+					title: String,
+					comments: [
+						{
+							body: String,
+							user: {
+								type: Schema.Types.ObjectId,
+								ref: "users",
+							},
+						},
+					],
+					labels: [{ body: String, color: String }],
+					attachments: [{ name: String, url: String }],
+				},
+			],
+		},
+	],
+});
+
+module.exports = Board = mongoose.model("board", BoardSchema);
