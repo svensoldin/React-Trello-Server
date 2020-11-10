a4958ba9cdd4e0f0a6e19d7fa383bbdfb7a4f336
Sven Soldin <soldinsven@gmail.com>
2020-11-10 10:45:00 +0100

    created patch request to /boards/:id/card to add a card to the board

--- routes/boards.js
+++ routes/boards.js
@@ -17,16 +17,22 @@ router.get("/", [auth], async (req, res) => {
 });
 
 // POST
-// Create a new board
+// Create a new board /boards
 
 router.post("/", [auth], async (req, res) => {
 	try {
-		const { title, photo, users, admins } = req.body;
-		const board = new Board({
+		const { title, photo, users, admins, columns } = req.body;
+		let board = new Board({
 			title,
 			photo,
 			users,
 			admins,
+		});
+		board.columns = columns.map((title) => {
+			return {
+				title,
+				cards: [],
+			};
 		});
 		await board.save();
 		return res.status(200).json(board);
@@ -37,19 +43,54 @@ router.post("/", [auth], async (req, res) => {
 });
 
 // DELETE
-// Delete a board
+// Delete a board /boards/:id
 
 router.delete("/:id", [auth], async (req, res) => {
 	try {
 		const board = await Board.findById(req.params.id);
 		if (!board) return res.status(403).json("Board not found");
-		if (board.admins.filter((admin) => req.user === admin).length) {
+
+		// Check if the user is an admin of the board
+		if (!board.admins.filter((admin) => req.user == admin).length) {
 			return res
 				.status(401)
 				.json("Not authorized, only an admin can delete a board");
 		}
+
 		await board.deleteOne();
 		return res.status(200).json("Board deleted");
+	} catch (err) {
+		console.error(err);
+		res.status(500).json("Server error");
+	}
+});
+
+// PATCH
+// Add a card to a board /boards/:id/card
+
+router.patch("/:id/card", [auth], async (req, res) => {
+	try {
+		const board = await Board.findById(req.params.id);
+		if (!board) return res.status(403).json("Board not found");
+		if (!board.admins.filter((admin) => req.user == admin).length) {
+			return res
+				.status(401)
+				.json("Not authorized, only an admin can add a card");
+		}
+		// Pull the right column from the board
+		let column = board.columns.filter(
+			(column) => req.body.column == column.title
+		)[0];
+		if (!column) return res.status(404).json("No such column");
+		const { title, labels, attachments, comments } = req.body.card;
+		column.cards.push({
+			title,
+			labels,
+			attachments,
+			comments,
+		});
+		await board.save();
+		return res.status(200).json("Card created");
 	} catch (err) {
 		console.error(err);
 		res.status(500).json("Server error");
