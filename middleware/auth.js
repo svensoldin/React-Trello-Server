dee9d6db2b248802407230ba0206357eb1df03dd
Sven Soldin <soldinsven@gmail.com>
2020-11-11 12:33:57 +0100

    created endpoint for drag'n drop of card from one column to another

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res, next) => {
 	const token = req.header("x-auth-token");
-	if (!token) return res.status(401).json("Not authorized, no token");
+	if (!token) return res.status(401).json("Not authenticated, no token");
 	try {
 		const decoded = await jwt.verify(token, process.env.JWT_SECRET);
 		req.user = decoded.id;
--- routes/boards.js
+++ routes/boards.js
@@ -48,12 +48,12 @@ router.post("/", [auth], async (req, res) => {
 router.delete("/:id", [auth], async (req, res) => {
 	try {
 		const board = await Board.findById(req.params.id);
-		if (!board) return res.status(403).json("Board not found");
+		if (!board) return res.status(400).json("Board not found");
 
 		// Check if the user is an admin of the board
 		if (!board.admins.filter((admin) => req.user == admin).length) {
 			return res
-				.status(401)
+				.status(403)
 				.json("Not authorized, only an admin can delete a board");
 		}
 
@@ -71,7 +71,7 @@ router.delete("/:id", [auth], async (req, res) => {
 router.patch("/:id/user", [auth], async (req, res) => {
 	try {
 		const board = await Board.findById(req.params.id);
-		if (!board) return res.status(403).json("Board not found");
+		if (!board) return res.status(400).json("Board not found");
 		board.users.push(req.body.user);
 		await board.save();
 		return res.status(200).json("User added");
@@ -87,10 +87,10 @@ router.patch("/:id/user", [auth], async (req, res) => {
 router.patch("/:id/user/remove", [auth], async (req, res) => {
 	try {
 		const board = await Board.findById(req.params.id);
-		if (!board) return res.status(404).json("No board found");
+		if (!board) return res.status(400).json("No board found");
 		// Check if the client is an admin
 		if (!board.admins.filter((admin) => req.user == admin).length) {
-			return res.status(401).json("Only an admin can remove a user");
+			return res.status(403).json("Only an admin can remove a user");
 		}
 		board.users = board.users.filter((user) => req.body.user != user);
 		await board.save();
@@ -107,7 +107,7 @@ router.patch("/:id/user/remove", [auth], async (req, res) => {
 router.patch("/:id/title", [auth], async (req, res) => {
 	try {
 		const board = await Board.findById(req.params.id);
-		if (!board) return res.status(403).json("Board not found");
+		if (!board) return res.status(400).json("Board not found");
 		board.title = req.body.title;
 		await board.save();
 		return res.status(200).json(board.title);
@@ -125,7 +125,7 @@ router.patch("/:id/title", [auth], async (req, res) => {
 router.patch("/:id/card", [auth], async (req, res) => {
 	try {
 		const board = await Board.findById(req.params.id);
-		if (!board) return res.status(403).json("Board not found");
+		if (!board) return res.status(400).json("Board not found");
 		// if (!board.admins.filter((admin) => req.user == admin).length) {
 		// 	return res
 		// 		.status(401)
@@ -133,10 +133,10 @@ router.patch("/:id/card", [auth], async (req, res) => {
 		// }
 
 		// Pull the right column from the board
-		let column = board.columns.filter(
+		let column = board.columns.find(
 			(column) => req.body.column == column.title
-		)[0];
-		if (!column) return res.status(404).json("No such column");
+		);
+		if (!column) return res.status(400).json("No such column");
 		const { title, labels, attachments, comments } = req.body.card;
 		column.cards.push({
 			title,
@@ -153,6 +153,44 @@ router.patch("/:id/card", [auth], async (req, res) => {
 });
 
 // PATCH
-// Add a comment
+// Drag and drop card from column to another /boards/:id/card/drag
+
+router.patch("/:id/card/drag", [auth], async (req, res) => {
+	try {
+		const board = await Board.findById(req.params.id);
+		if (!board) return res.status(400).json("Board not found");
+
+		// Pull the drag column from the board
+		let dragColumn = board.columns.find(
+			(column) => req.body.dragColumn == column.title
+		);
+		if (!dragColumn) return res.status(400).json("Drag column not found");
+
+		// Pull the drop column from the board
+		let dropColumn = board.columns.find(
+			(column) => req.body.dropColumn == column.title
+		);
+		if (!dropColumn) return res.status(400).json("Drop column not found");
+
+		// Pull the card from the drag column
+		const draggedCard = dragColumn.cards.find(
+			(card) => req.body.cardId == card._id
+		);
+		if (!draggedCard) return res.status(400).json("Card not found");
+
+		// Add the card to the drop column
+		dropColumn.cards.push(draggedCard);
+
+		// Remove the card from the drag column
+		dragColumn.cards = dragColumn.cards.filter((card) => draggedCard != card);
+
+		// Save board
+		await board.save();
+		return res.status(200).json("Card successfully moved");
+	} catch (err) {
+		console.error(err);
+		return res.status(500).json("Server error");
+	}
+});
 
 module.exports = router;
