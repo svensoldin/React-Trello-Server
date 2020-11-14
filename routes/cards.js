58e8a049c2b21002875789fe343dafa196fb727c
Sven Soldin <soldinsven@gmail.com>
2020-11-14 12:29:33 +0100

    added routes for downloading and deleting attachments

--- routes/cards.js
+++ routes/cards.js
@@ -4,6 +4,7 @@ const Card = require("../models/Card");
 const Board = require("../models/Board");
 const auth = require("../middleware/auth");
 const multer = require("multer");
+const fs = require("fs");
 
 // Multer config
 
@@ -172,6 +173,79 @@ router.post(
 		} catch (err) {
 			console.error(err);
 			return res.status(500);
+		}
+	}
+);
+
+// GET
+// Download an attachment /cards/:cardId/attachments/:attachmentId/download
+
+router.get(
+	"/:cardId/attachments/:attachmentId/download",
+	[auth],
+	async (req, res) => {
+		try {
+			const card = await Card.findById(req.params.cardId);
+			if (!card) return res.status(400).json("Card not found");
+
+			const board = await Board.findById(card.board);
+			if (!board) return res.status(400).json("Board not found");
+
+			// Check if client is a user of the board
+			if (!board.users.find((user) => req.user == user))
+				return res
+					.status(403)
+					.json("Only users of the board can download attachments");
+
+			const attachmentToDownload = card.attachments.find(
+				(attachment) => attachment._id == req.params.attachmentId
+			);
+			if (!attachmentToDownload)
+				return res.status(400).json("Attachment not found");
+			const file = `./uploads/${attachmentToDownload.fileName}`;
+			res.status(200).download(file);
+		} catch (err) {
+			console.error(err);
+			res.status(500).json(err.message);
+		}
+	}
+);
+
+// PATCH
+// Delete an attachment /cards/:cardId/attachments/:attachmentId/delete
+
+router.patch(
+	"/:cardId/attachments/:attachmentId/delete",
+	[auth],
+	async (req, res) => {
+		try {
+			const card = await Card.findById(req.params.cardId);
+			if (!card) return res.status(400).json("Card not found");
+
+			const board = await Board.findById(card.board);
+			if (!board) return res.status(400).json("Board not found");
+
+			// Check if client is a user of the board
+			if (!board.users.find((user) => req.user == user))
+				return res
+					.status(403)
+					.json("Only users of the board can remove attachments");
+
+			const attachmentToDelete = card.attachments.find(
+				(attachment) => attachment._id == req.params.attachmentId
+			);
+			if (!attachmentToDelete)
+				return res.status(400).json("Attachment not found");
+			// Delete the corresponding file from disk
+			fs.unlinkSync(`./uploads/${attachmentToDelete.fileName}`);
+			// Remove the attachment from card
+			const removeIndex = card.attachments.indexOf(attachmentToDelete);
+			card.attachments.splice(removeIndex, 1);
+			await card.save();
+			return res.status(200).json(card.attachments);
+		} catch (err) {
+			console.error(err);
+			res.status(500).json(err.message);
 		}
 	}
 );
